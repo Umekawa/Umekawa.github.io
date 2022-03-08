982dd1c255b691fce3d4e315e338e2c7dca756f1
Umekawa <umekawana@gmail.com>
2022-03-09 00:12:09 +0900

    Add dialy

--- script/create_today_diary.rb
+++ script/create_today_diary.rb
@@ -4,5 +4,5 @@ year = now.strftime('%Y')
 month = now.strftime('%m')
 day = now.strftime('%d')
 File.open("./src/markdown/#{year}#{month}#{day}.md", mode = "w"){|f|
-  f.write("---\nid: \"#{year}年#{month}月#{day}日\"\ntitle: \"#{year}年#{month}月#{day}日\"\ndate: \"#{year}-#{month}-#{day}\"\n---\n")
+  f.write("---\nid: \"#{year}#{month}#{day}\"\ntitle: \"#{year}年#{month}月#{day}日\"\ndate: \"#{year}-#{month}-#{day}\"\n---\n")
 }
